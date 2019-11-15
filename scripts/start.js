f2e2a7b3010aec3304083a175a9b37d959e6e3e5
maomao1996 <1714487678@qq.com>
2019-11-15 20:54:18 +0800

    chore：修改默认启动端口，去除 lock 文件，更新文档

--- scripts/start.js
+++ scripts/start.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
@@ -41,7 +39,7 @@ if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
 }
 
 // Tools like Cloud9 rely on this.
-const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 3000;
+const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 8163;
 const HOST = process.env.HOST || '0.0.0.0';
 
 if (process.env.HOST) {
